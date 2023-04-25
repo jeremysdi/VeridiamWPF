bf8a58d62b773e6e40e5a7d23cd33171d8170845
scott <scottjreeves@hotmail.com>
2023-04-25 13:22:18 -0700

    current

--- Veridiam/DataRecord.cs
+++ Veridiam/DataRecord.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +9,71 @@ namespace Veridiam
 {
     internal class DataRecord
     {
+        public DataRecord() 
+        {
+           
+            
+        }
     }
+
+    internal class MechConfig
+    {
+        public int numberOfAxis { get; }
+        public string[] axisDesignation { get; set; }
+        public int[] acceleration { get; set; }
+        public int[] deceleration { get; set; }
+        public double[] scalingDivider { get; set; }
+        public int[] p { get; set; }
+        public int[] i { get; set; }
+        public int[] d { get; set; }
+        public bool[] axisUseLimits { get; set; }
+
+        public MechConfig(int numberOfAxis) 
+        {
+            this.numberOfAxis = numberOfAxis;
+            axisDesignation = new string[numberOfAxis];
+            acceleration = new int[numberOfAxis];
+            deceleration = new int[numberOfAxis];
+            scalingDivider = new double[numberOfAxis];
+            p = new int[numberOfAxis];
+            i = new int[numberOfAxis];
+            d = new int[numberOfAxis];
+            axisUseLimits = new bool[numberOfAxis];
+            LoadDefaults();
+        }
+
+        private void LoadDefaults()
+        {
+            try
+            {
+                FileStream fileStream = new FileStream("default.cfg", FileMode.Open);
+                while (fileStream.Position < fileStream.Length)
+                {
+                    // load whatever
+                }
+            }
+            catch (FileNotFoundException) 
+            { 
+                // default file not found, load hard-coded defaults
+            }
+        }
+    }
+
+    internal class ScanParameters
+    {
+        public ScanParameters()
+        {
+            // Scan Speed
+            // Index Axis Increment
+            // Scan Axis Increment
+            // Part Length
+            // Part Diameter
+        }
+    }
+
+    // Other
+        // Limit status
+        // Current Position
+        // Scan start position
+        // Scan end position
 }
