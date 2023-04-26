991826331f2bc8dd27cd3a8229a8912ddc77eb2e
scott <scottjreeves@hotmail.com>
2023-04-25 18:31:56 -0700

    current

--- Veridiam/DataRecord.cs
+++ Veridiam/DataRecord.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 
@@ -11,15 +12,17 @@ namespace Veridiam
     internal class DataRecord
     {
         MechConfig MechConfig { get; }
+        ScanParameters ScanParameters { get; }
+        ScanData ScanData { get; }
+        SystemStatus Status { get; }
         public DataRecord(int numberOfAxis, int numberOfTransducers)
         {
             MechConfig = new MechConfig(numberOfAxis);
-            // Limit status
-            // Current Position
 
         }
     }
 
+    // Values for setting up motion
     internal class MechConfig
     {
         public int NumberOfAxis { get; }
@@ -116,41 +119,153 @@ namespace Veridiam
         }
     }
 
+    // Values for setting up scan
     internal class ScanParameters
     {
+        public int ScanSpeed { get; set; }
+        public double IndexIncrement { get; set; }
+        public double ScanIncrement { get; set; }
+        public double PartLength { get; set; }
+        public double PartDiameter { get; set; }
+        public double[] ScanStartPosition { get; set; }
+        public double[] ScanEndPosition { get; set; }
+
         public ScanParameters()
         {
-            // Scan Speed
-            // Index Axis Increment
-            // Scan Axis Increment
-            // Part Length
-            // Part Diameter
-            // Scan start position
-            // scan end position
+            
         }
     }
 
+    // ScanData manages a data file that has raw data, used to write data during scan
     internal class ScanData
     {
         private StreamWriter FileWriter = null;
-        public ScanData(int numberOfDataPoints)
+        private readonly string defaultFileName = "data.frm";
+        private readonly object WriteScanLock = new object();
+        private bool WritingScanData = false;
+        private int writeCount = 0;
+        private Queue<ushort[]> WriteDataQueue = new Queue<ushort[]>();
+        private Task WriteTask = null;
+        public ScanData()
         {
             // write it all to a data file, only header is the number of dataPoints
-            FileStream fileStream = new FileStream("data.frm", FileMode.Create);
+            
+        }
+
+        private void FileWriteTask()
+        {
+            FileStream fileStream = new FileStream(defaultFileName, FileMode.Create);
             FileWriter = new StreamWriter(fileStream);
-            FileWriter.Write(numberOfDataPoints);
+
+            while (WritingScanData)
+            {
+                if (WriteDataQueue.Count > 0)
+                {
+                    ushort[] frame = WriteDataQueue.Dequeue();
+                    FileWriter.Write(frame.Length);
+                    foreach (ushort i in frame) { FileWriter.Write(i); }
+                }
+            }
+
+            FileWriter.Flush();
+            FileWriter.Close();
+        }
+
+        public void ScanStart()
+        {
+            lock (WriteScanLock)
+            {
+                if (!WritingScanData)
+                {
+                    WritingScanData = true;
+                    writeCount = 0;
+                    // run write scan data task
+                    WriteTask = Task.Run(() => FileWriteTask());
+                }
+            }
+            
+            // else send message, already scanning
         }
 
         public void WriteData(ushort[] frame)
         {
-            FileWriter.Write(frame.Length);
-            foreach (ushort i in frame) { FileWriter.Write(i); }
+            lock (WriteScanLock)
+            {
+                if (WritingScanData)
+                {
+
+                    writeCount++;
+                }
+            }
+            
+            // else send message, not currently scanning
         }
 
         public void ScanComplete()
         {
-            FileWriter.Flush();
-            FileWriter.Close();
+            if (WritingScanData)
+            {
+                
+                WritingScanData = false;
+            }
+            // else send message, not currently scanning
+
+            // can now read/compile 
+        }
+    }
+
+    // SystemStatus keeps track of data that is checked frequently, using locks for multithreaded access
+        // -- Limit Status
+        // -- Current Position
+    internal class SystemStatus
+    {
+        private readonly object LimitLock = new object();
+        private readonly object PositionLock = new object();
+
+        private int NumberOfAxis;
+        private bool[] LimitStatus = null;
+        private double[] CurrentPosition = null;
+        public SystemStatus(int numberOfAxis) 
+        { 
+            NumberOfAxis = numberOfAxis;
+            LimitStatus = new bool[NumberOfAxis];
+            CurrentPosition = new double[NumberOfAxis];
+        }
+
+        public bool[] GetLimitStatus()
+        {
+            bool[] copyLimitStatus = new bool[NumberOfAxis];
+            lock (LimitLock)
+            {
+                Array.Copy(LimitStatus, copyLimitStatus, NumberOfAxis);
+            }
+            return copyLimitStatus;
+        }
+
+        public void SetLimitStatus(bool[] limitStatus) 
+        { 
+            lock (LimitLock)
+            {
+                LimitStatus = limitStatus;
+            }
+        }
+
+        public double[] GetCurrentPosition()
+        {
+            double[] copyCurrentPosition = new double[NumberOfAxis];
+            lock (PositionLock)
+            {
+                Array.Copy(CurrentPosition, copyCurrentPosition, NumberOfAxis);
+            }
+            return copyCurrentPosition;
+        }
+
+        public void SetCurrentPosition(double[] currentPosition)
+        {
+            lock (PositionLock)
+            {
+                CurrentPosition = currentPosition;
+            }
         }
     }
 
